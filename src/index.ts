f05a2ace7f05a7902b08f66f238aadf62b4810ab
Saiya <okarl@outlook.com>
2024-06-15 17:59:59 +0800

    feat: prettify code

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,96 @@
 import { parseDocument } from 'htmlparser2';
 import { Element, Text, Node } from 'domhandler';
 
-// 将特殊属性从 HTML 转换为 React
-const attributeMap: Record<string, string> = {
+/**
+ * jsx special attributes mapping
+ */
+const jsxAttributeMap: Record<string, string> = {
   'class': 'className',
   'for': 'htmlFor',
 };
-
+// random tag and prop to replace the placeholder tag, to avoid conflict with the original tag
 const TAG_PROP = `data-${crypto.randomUUID()}`;
 const TEMP_TAG = `tag-${crypto.randomUUID()}`;
+
 /**
- * 将 HTML 属性转换为 React 属性
- * @param attributes HTML 属性对象
- * @returns React 属性对象
+ * convert HTML attributes to React attributes
+ * @param attributes attributes object
+ * @returns React attributes object
  */
 function convertAttributes(attributes: Record<string, string>): Record<string, string> {
   const convertedAttributes: Record<string, string> = {};
-  for (const [key, value] of Object.entries(attributes)) {
-    let newKey = attributeMap[key] || key;
-    let newValue = value;
 
-    // Convert template placeholders to JSX expressions
+  for (const [key, value] of Object.entries(attributes)) {
+    let newKey = jsxAttributeMap[key] || key;
+    let newValue = value;
     if (/{\w+}/.test(value)) {
-      newValue = `{props.${value.slice(1, -1)}}`;
+      const parts = value.split(/({\w+})/g);
+      const convertedParts = parts.map(part => {
+        if (part.startsWith('{') && part.endsWith('}')) {
+          const prop = part.slice(1, -1); // remove the curly braces
+          return standardizeProp(prop);
+        } else {
+          if (part === '') return null;
+          return `'${part}'`;
+        }
+      });
+      newValue = `{${convertedParts.filter(Boolean).join(' + ')}}`;
     } else {
-      // Wrap string values in quotes
       newValue = `"${newValue}"`;
     }
 
     convertedAttributes[newKey] = newValue;
   }
+
   return convertedAttributes;
 }
 
 /**
- * 创建 React 元素字符串
- * @param tag 标签名
- * @param attributes 属性对象
- * @param children 子元素字符串
- * @returns React 元素字符串
+ * standardize the prop reference 
+ * @param prop prop name
+ */
+function standardizeProp(prop: string): string {
+  // valid variable name
+  if (/^[$a-z][\da-z]*$/i.test(prop)) {
+    return `props.${prop}`;
+  }
+  return `props['${prop}']`;
+}
+
+/**
+ * create React element string
+ * @param tag tag name
+ * @param attributes attributes object
+ * @param children children string
+ * @returns React element string
  */
 function createElementString(tag: string, attributes: Record<string, string>, children: string): string {
   const attrString = Object.entries(attributes)
     .map(([key, value]) => `${key}=${value}`)
-    .join(' ');
+    .join(' ').trim();
 
+  const openTag = attrString ? `${tag} ${attrString}` : `${tag}`;
   if (children) {
-    return `<${tag} ${attrString}>${children}</${tag}>`;
+    return `<${openTag}>${children}</${tag}>`;
   } else {
-    return `<${tag} ${attrString} />`;
+    return `<${openTag}/>`;
   }
 }
 
 /**
- * 递归地将 DOM 节点转换为 React 组件字符串
- * @param node DOM 节点
- * @returns React 组件字符串
+ * convert DOM node to React component string recursively
+ * @param node DOM node
+ * @returns React component string
  */
 function convertNode(node: Node): string {
   if (node instanceof Text) {
-    return node.data.replace(/{(\w+)}/g, '{props.$1}');
+    return node.data.replace(/{(\w+)}/g, (_, $1) => `{${standardizeProp($1)}}`);
   }
 
   if (node instanceof Element) {
     let tag = node.name;
     if (tag === TEMP_TAG) {
-      tag = `{props.${node.attribs[TAG_PROP]}}`;
+      tag = `{${standardizeProp(node.attribs[TAG_PROP])}}`;
       delete node.attribs[TAG_PROP];
     }
 
@@ -79,31 +104,34 @@ function convertNode(node: Node): string {
 }
 
 /**
- * 替换模板中的占位符标签名
- * @param template 模板字符串
- * @returns 替换后的模板字符串
+ * replace tag placeholders with temporary tag and prop
+ * @param template template string
+ * @returns replaced template string
  */
 function replaceTagPlaceholders(template: string): string {
   return template.replace(/<\{(\w+)\}/g, `<${TEMP_TAG} ${TAG_PROP}="$1"`).replace(/<\/\{(\w+)\}>/g, `</${TEMP_TAG}>`);
 }
 
 /**
- * 将模板字符串转换为 React 组件字符串
- * @param template 模板字符串
- * @returns React 组件字符串
+ * convert template string to React component string
+ * @param template template string
+ * @returns React component string
  */
 function templateToReactComponent(template: string): string {
   const transformedTemplate = replaceTagPlaceholders(template);
   const document = parseDocument(transformedTemplate);
   const componentBody = document.children.map(convertNode).join('');
 
-  return `function R(props) { return <>${componentBody}</>; }`;
+  // if the template has only one root node, no need for extra <> </>
+  const needsFragmentWrapper = document.children.length > 1;
+
+  return `function R(props) { return ${needsFragmentWrapper ? `<>${componentBody}</>` : componentBody}; }`;
 }
 
 // 示例使用
-const template = `Hello, {user}! You Got 
+const template = `<span>Hello, {user}! You Got 
 <{p1} class="{className}" data-id="{id} haha">{score}</{p1}>!
 <{w31} class="{className}" data-id="{id}">{score}</{w31}>!
-<span class="hilight">Great!</span>`;
+<span class="hilight">Great!</span></span>`;
 const reactComponentString = templateToReactComponent(template);
 console.log(reactComponentString);
